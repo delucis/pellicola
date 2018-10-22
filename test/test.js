c095ec3852835250c5713a4ef945335d0a64f390
delucis <swithinbank@gmail.com>
2018-10-21 23:00:52 -0400

    feat: Initial commit of core functionality
    Module now provides a function which takes a canvas sketch function and generates a video

---
+++ test/test.js
@@ -0,0 +1,20 @@
+import test from 'ava'
+import fs from 'fs'
+import { directory } from 'tempy'
+import m from '../lib/pellicule'
+
+test('imports module', test => {
+  test.is(typeof m, 'function')
+})
+
+test.cb('generates a video', test => {
+  const sketch = () => ({ context: ctx, height, width, playhead }) => {
+    ctx.fillStyle = '#fff'
+    ctx.fillRect(0, 0, width, height)
+    ctx.fillStyle = '#000'
+    ctx.fillRect(0, 0, width * playhead, height)
+  }
+
+  m(sketch, { duration: 1, outDir: directory() })
+    .then(src => fs.readFile(src, test.end))
+})
