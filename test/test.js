3343a4492fe2a7bfaeb93362fdfbbe76cab4589f
delucis <swithinbank@gmail.com>
2018-10-22 21:26:41 -0400

    test: Add custom font loading test

--- test/test.js
+++ test/test.js
@@ -18,3 +18,21 @@ test.cb('generates a video', test => {
   m(sketch, { duration: 1, outDir: directory() })
     .then(src => fs.readFile(src, test.end))
 })
+
+test.cb('can load custom fonts', test => {
+  const fonts = [
+    { family: 'testFont', path: 'test/test-regular.ttf' },
+    { family: 'testFont', weight: 'bold', path: 'test/test-bold.ttf' }
+  ]
+
+  const sketch = () => ({ context: ctx, height, width }) => {
+    ctx.fillStyle = '#fff'
+    ctx.font = '400 36px testFont'
+    ctx.fillText('Regular text', 0, 36)
+    ctx.font = '700 24px testFont'
+    ctx.fillText('Smaller bold text', 0, 100)
+  }
+
+  m(sketch, { duration: 0.25, outDir: directory(), fonts })
+    .then(src => fs.readFile(src, test.end))
+})
