02de38ed743adcd7f3204a88d07fce13f1cc454d
Chris Swithinbank <swithinbank@gmail.com>
2018-10-23 13:45:46 -0400

    test: Improve coverage (#2)
    * test: Add custom font loading test
    
    * test: Add tests for bad arguments to FrameMaker
    
    * test: Add test for missing arguments to pellicola
    
    * chore(README): Update Coveralls badge to reference correct branch
    
    * test: Add test for sketch functions that return objects
    
    * test: Add test for conflicting duration and totalFrames settings
    
    * test: Add tests for totalFrames option and writing frames as JPEGs
    
    * chore: Update test fonts to include lowercase letters
    
    * test: Refactor tests that check file was written
    
    * test: Refactor tests that just need an empty sketch function
    
    * test: Add test for writing to a custom filename
    
    * test: Add test for using a custom framerate
    
    * test: Suppress console output on warning test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,16 @@
-import test from 'ava'
+import ava from 'ava'
+import ninos from 'ninos'
 import fs from 'fs'
+import path from 'path'
 import { directory } from 'tempy'
 import m from '../lib/pellicola'
+
+const test = ninos(ava)
+
+const emptySketch = () => () => {}
+
+const cb = (sketch, opts, test) => m(sketch, opts)
+  .then(src => fs.readFile(src, test.end))
 
 test('imports module', test => {
   test.is(typeof m, 'function')
@@ -15,6 +24,74 @@ test.cb('generates a video', test => {
     ctx.fillRect(0, 0, width * playhead, height)
   }
 
-  m(sketch, { duration: 1, outDir: directory() })
-    .then(src => fs.readFile(src, test.end))
+  cb(sketch, { duration: 1, outDir: directory() }, test)
+})
+
+test.cb('can load custom fonts', test => {
+  const fonts = [
+    { family: 'testFont', path: 'test/test-regular.ttf' },
+    { family: 'testFont', weight: 'bold', path: 'test/test-bold.ttf' }
+  ]
+
+  const sketch = () => ({ context: ctx, height, width }) => {
+    ctx.fillStyle = '#fff'
+    ctx.font = '400 36px testFont'
+    ctx.fillText('Regular text', 0, 36)
+    ctx.font = '700 24px testFont'
+    ctx.fillText('Smaller bold text', 0, 100)
+  }
+
+  cb(sketch, { duration: 0.25, outDir: directory(), fonts }, test)
+})
+
+test('throws if provided neither totalFrames nor duration', async test => {
+  await test.throws(m(emptySketch), TypeError)
+})
+
+test('FrameMaker throws if provided an invalid frame format', async test => {
+  await test.throws(
+    m(emptySketch, { duration: 0.25, frameFormat: 'bmp' }),
+    RangeError
+  )
+})
+
+test('FrameMaker throws if provided an unuseable sketch argument', async test => {
+  await test.throws(
+    m(() => 'i am a function but i return a string :-(', { duration: 0.25 }),
+    TypeError
+  )
+})
+
+test.cb('FrameMaker can use a sketch that returns an object', test => {
+  const sketch = () => ({
+    render: () => {}
+  })
+  cb(sketch, { duration: 0.25, outDir: directory() }, test)
+})
+
+test('non-matching duration & totalFrames should raise a warning', async test => {
+  const spy = test.context.spy(console, 'warn', () => {})
+  await m(emptySketch, { duration: 1, totalFrames: 12, outDir: directory() })
+  test.is(spy.calls.length, 1)
+})
+
+test.cb('duration can be set using totalFrames option', test => {
+  cb(emptySketch, { totalFrames: 6, outDir: directory() }, test)
+})
+
+test.cb('can write frames as JPEGs', test => {
+  cb(emptySketch, { frameFormat: 'jpeg', totalFrames: 6, outDir: directory() }, test)
+})
+
+test.cb('can set a custom filename', test => {
+  const filename = 'custom-name.mp4'
+  m(emptySketch, { filename, totalFrames: 6, outDir: directory() })
+    .then(src => {
+      test.is(path.basename(src), filename)
+      fs.readFile(src, test.end)
+    })
+})
+
+test.cb('can use a custom framerate', test => {
+  cb(emptySketch, { fps: 12, totalFrames: 6, outDir: directory() }, test)
 })
