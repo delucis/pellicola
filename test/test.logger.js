832e7f04f0b671dd1149b5a97ea4e77ec8968893
delucis <swithinbank@gmail.com>
2018-11-04 14:46:48 -0500

    feat(logger): Write an ora wrapper to make silent output easy

---
+++ test/test.logger.js
@@ -0,0 +1,24 @@
+import test from 'ava'
+import logger from '../lib/logger'
+
+test('it can return an ora stub', t => {
+  const silentOra = logger({ silent: true })
+  t.is(typeof silentOra, 'function')
+  t.is(typeof silentOra.promise, 'function')
+  const instance = silentOra()
+  t.is(typeof instance.start, 'function')
+  t.is(typeof instance.stop, 'function')
+  t.is(typeof instance.succeed, 'function')
+  t.is(typeof instance.fail, 'function')
+})
+
+test('it can return ora unmodified', t => {
+  const ora = logger()
+  t.is(typeof ora, 'function')
+  t.is(typeof ora.promise, 'function')
+  const instance = ora()
+  t.is(typeof instance.start, 'function')
+  t.is(typeof instance.stop, 'function')
+  t.is(typeof instance.succeed, 'function')
+  t.is(typeof instance.fail, 'function')
+})
