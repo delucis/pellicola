e5078622a19b56a371270526e9c8c3c7bfa72532
delucis <swithinbank@gmail.com>
2020-01-26 14:56:55 +0100

    test: Use expectation objects for throws assertions

--- test/test.js
+++ test/test.js
@@ -84,7 +84,10 @@ test.cb('can load custom fonts', test => {
 })
 
 test('throws if provided neither totalFrames nor duration', async test => {
-  await test.throwsAsync(m(emptySketch, { silent: true }), TypeError)
+  await test.throwsAsync(
+    m(emptySketch, { silent: true }),
+    { instanceof: TypeError }
+  )
 })
 
 test('throws if totalFrames is less than initial frame', async test => {
@@ -94,14 +97,14 @@ test('throws if totalFrames is less than initial frame', async test => {
 test('FrameMaker throws if provided an invalid frame format', async test => {
   await test.throwsAsync(
     m(emptySketch, { duration: 0.25, frameFormat: 'bmp', silent: true }),
-    RangeError
+    { instanceof: RangeError }
   )
 })
 
 test('FrameMaker throws if provided an unuseable sketch argument', async test => {
   await test.throwsAsync(
     m(() => 'i am a function but i return a string :-(', { duration: 0.25, silent: true }),
-    TypeError
+    { instanceof: TypeError }
   )
 })
 
